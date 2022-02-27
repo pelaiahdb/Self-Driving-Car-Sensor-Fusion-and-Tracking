8d22ff69e805ad6954079078b9b7116822f11e48
defloored <p.b@berkeley.edu>
2022-02-27 13:27:01 -0800

    Midterm part 1 finished

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -40,15 +40,33 @@ def show_pcl(pcl):
 
     # step 1 : initialize open3d with key callback and create window
 
+    vis = o3d.visualization.VisualizerWithKeyCallback()
+    key = 262
+    vis.create_window()
+
+
+    def o3d_update_function():
+        vis.update_renderer()
+        vis.poll_events()
+        vis.run()
 
     
     # step 2 : create instance of open3d point-cloud class
+    cloud = o3d.geometry.PointCloud()
 
     # step 3 : set points in pcd instance by converting the point-cloud into 3d vectors (using open3d function Vector3dVector)
+    cloud.points = o3d.utility.Vector3dVector(pcl[:,:3])
+
 
     # step 4 : for the first frame, add the pcd instance to visualization using add_geometry; for all other frames, use update_geometry instead
+    vis.add_geometry(cloud)
     
     # step 5 : visualize point cloud and keep window open until right-arrow is pressed (key-code 262)
+    
+    vis.register_key_callback(key, o3d_update_function)
+
+    vis.run()
+    vis.update_renderer()
 
     #######
     ####### ID_S1_EX2 END #######     
